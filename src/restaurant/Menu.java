abde9d01ca9c474f7c9b8cc158434092c6083c55
Andrew Pomerleau <arpomerleau@gmail.com>
2022-07-25 20:52:25 -0400

    Completed basic structure for app.

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,57 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Menu {
+    ArrayList<MenuItem> items;
+
+    public Menu() {
+        items = new ArrayList<>();
+    }
+
+    public Menu(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+
+    public Menu(MenuItem[] items) {
+        this.items = new ArrayList<>(Arrays.asList(items));
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public void removeItem(MenuItem item) {
+        items.remove(item);
+    }
+
+    public void listItems() {
+        System.out.println("::: MENU :::");
+
+        for (String cat : MenuItem.CATEGORIES) {
+            System.out.printf("::: %s :::\n", cat);
+
+            for (MenuItem item : items) {
+                if (item.getCategory().equals(cat)) {
+                    System.out.println(item.display());
+                }
+            }
+        }
+    }
+
+    public static Menu getSampleMenu() {
+        Menu menu = new Menu();
+
+        menu.addItem(new MenuItem("Potato Skins", "Baked Potato Skins topped with cheese, bacon crumbles, and chives. Served with Sour Cream.", MenuItem.APPETIZER, 7.99));
+        menu.addItem(new MenuItem("Mozzarella Sticks", "Breaded and fried mozzarella sticks. Served with Marinara dipping sauce.", MenuItem.APPETIZER, 8.99));
+
+        menu.addItem(new MenuItem("Chicken Pesto", "Angel hair pasta topped with basil pesto sauce and grilled chicken.", MenuItem.MAIN_COURSE, 12.99));
+        menu.addItem(new MenuItem("Cowboy Burger", "Quarter pound hamburger with provolone cheese, bacon and house barbeque sauce.", MenuItem.MAIN_COURSE, 9.99));
+
+        menu.addItem(new MenuItem("Chocolate Cake", "Dark chocolate cake with buttercream frosting.", MenuItem.DESSERT, 4.99));
+        menu.addItem(new MenuItem("Strawberry Shortcake", "Fluffy shortcake topped with whipped cream and strawberries.", MenuItem.DESSERT, 5.99));
+
+        return menu;
+    }
+}
---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,118 @@
+package restaurant;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class MenuItem {
+    public static final String MAIN_COURSE = "Main Course", APPETIZER = "Appetizer", DESSERT = "Dessert";
+    public static final ArrayList<String> CATEGORIES = new ArrayList<>(Arrays.asList(APPETIZER, MAIN_COURSE, DESSERT));
+    private static final DecimalFormat DF = new DecimalFormat("0.00");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM-dd-yyyy");
+    private static final long DAY_IN_MS = 1000 * 60 * 60 * 24;
+    private String name, description, category;
+    private double price;
+    private final Date created;
+    private Date updated;
+
+
+    public MenuItem() {
+        this.name = "Name";
+        this.category = MAIN_COURSE;
+        this.description = "Description";
+        this.price = 0.00;
+
+        created = getToday();
+        updated = getToday();
+    }
+
+    public MenuItem(String name, String description, String category, double price) {
+        this.name = name;
+        this.description = description;
+        setCategory(category);
+        this.price = price;
+
+        created = getToday();
+        updated = getToday();
+    }
+
+    public String display() {
+        if (isNew()) {
+            return String.format("(NEW) %s : %s\n  ($%s)", name, description, DF.format(price));
+        }
+        return String.format("%s : %s\n  ($%s)", name, description, DF.format(price));
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        if (!CATEGORIES.contains(category)) {
+            return;
+        }
+
+        this.category = category;
+        this.updated = getToday();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        this.updated = getToday();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updated = getToday();
+    }
+
+    public boolean isNew() {
+        Date cutoff = oneWeekBefore(getToday());
+        return created.after(cutoff) || updated.after(cutoff);
+    }
+
+    private static Date parseDate(String date) {
+        try {
+            return DATE_FORMAT.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Date oneWeekBefore(Date date) {
+        return new Date(date.getTime() - (7 * DAY_IN_MS));
+    }
+
+    private static Date getToday() {
+        try {
+            return DATE_FORMAT.parse(String.format(
+                    "%2d-%2d-%4d",
+                    LocalDate.now().getMonthValue(),
+                    LocalDate.now().getDayOfMonth(),
+                    LocalDate.now().getYear()
+            ));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
